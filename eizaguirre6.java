15aa41f05e17f99e695e3b5aad3983f2ffc6ecac
Jesus-Little-Helper <cscuser33@ip-172-31-14-107.us-east-2.compute.internal>
2026-02-18 03:17:39 +0000

    lab 6

---
+++ eizaguirre6.java
@@ -0,0 +1,50 @@
+/*Name: Esthepanie Izaguirre
+ * CSC 321
+ * Lab 06 */
+
+
+import java.util.Scanner;
+
+public class eizaguirre6
+{
+	public static void main(String[] args)
+	{
+		int x = 40;
+		int y = 6;
+
+		double w = 40;
+		double z = 6; 
+
+		int total_1 = 0;
+		int total_2 = 0;
+		int total_3 = 0;
+
+		double vtotal_1 = 0;
+		double vtotal_2 = 0;
+		double vtotal_3 = 0;
+
+		total_1  =  x + y * x / y - x;
+		vtotal_1  = w + z * w / z - w;
+
+		 total_2  = - x - y / x * y + x;
+                vtotal_2  = - w - z / w * z + w;
+
+		 total_3  = x + y - x / y;
+                vtotal_3  = w + z - w / z;
+
+
+		System.out.println("---- Expression 1 ----");
+		System.out.println("int total = " + total_1);
+		System.out.println("double total = " + vtotal_1);
+
+		System.out.println("---- Expression 2 ----");
+		 System.out.println("int total = " + total_2);
+                System.out.println("double total = " + vtotal_2);
+
+		System.out.println("---- Expression 3 ----");
+		 System.out.println("int total = " + total_3);
+                System.out.println("double total = " + vtotal_3);
+
+
+	}
+}
