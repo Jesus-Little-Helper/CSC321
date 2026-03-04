dd09593242873660829a9d6f8c683026eb7589b9
Jesus-Little-Helper <cscuser33@ip-172-31-14-107.us-east-2.compute.internal>
2026-03-04 01:36:41 +0000

    lab #7

---
+++ eizaguirre7.py
@@ -0,0 +1,36 @@
+#Name: Esthepanie Izaguirre
+#CSC 321 
+#Lab 07
+
+
+#Variables
+w = "Esthepanie"    #String
+x = 9       #int
+y = 5.5     #Float
+
+
+#Variable Comparison
+#int
+if x > 5:
+    print("X should be GREATER than 7")
+elif x == 5:
+    print("X is EQUALS to 7")
+else:
+    print("X is LESS than 7")
+
+#float
+if w == "Esthepanie":
+    print("The NAme is Esthepanie")
+elif w < "Esthepanie":
+    print("The w comes before Esthepanie")
+else:
+    print("The nam ecomes after Esthepanie")
+
+#Float
+if y > 5.5:
+    print("Y should be GREATER than 5.5")
+elif y == 5.5:
+    print("Y is EQUALS to 5.5")
+else:
+       print("Y is LESS than 5.5")
+
